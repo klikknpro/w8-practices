959870e4376840b2d7181fb01a5fdd53b781448d
klikknpro <80091502+klikknpro@users.noreply.github.com>
2021-12-07 10:41:10 +0100

    callback -NOT good demo

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-function init() {}
+function init() {
+  const multiply = function (callback) {
+    return callback(2, 3) * 10;
+  };
+
+  const add = function (a, b) {
+    return a + b;
+  };
+
+  const sub = function (a, b) {
+    return a - b;
+  };
+
+  console.log(multiply(add));
+}
 
 window.addEventListener("load", init);
-
-/*
-window.addEventListener("load", function(){
-});
-window.addEventListener("load", () => {
-});
-*/
